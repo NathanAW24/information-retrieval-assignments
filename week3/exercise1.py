e5b47c17fed7d17463d9fa210ccff660fb2266a0
NathanAW24 <nathan_wiryawan@mymail.sutd.edu.sg>
2024-10-02 22:00:03 +0800

    exercise 1 done

--- week3/exercise1.py
+++ week3/exercise1.py
@@ -1,6 +1,7 @@
+from typing import Literal
 import pandas as pd
 
-index = ['car', 'auto', 'insurance', 'best']
+indices = ['car', 'auto', 'insurance', 'best']
 
 table1 = {
     'Doc1': [27, 3, 0, 14],
@@ -9,7 +10,7 @@ table1 = {
 }
 
 
-df_table1 = pd.DataFrame(table1, index=index)
+df_table1 = pd.DataFrame(table1, index=indices)
 
 # print(df_table1)
 
@@ -17,6 +18,14 @@ table2 = {
     'idf': [1.65, 2.08, 1.62, 1.5]
 }
 
-df_table2 = pd.DataFrame(table2, index=index)
+df_table2 = pd.DataFrame(table2, index=indices)
 
 # print(df_table2)
+
+def tf_idf(doc: Literal['Doc1', 'Doc2', 'Doc3'], index: Literal['car', 'auto', 'insurance', 'best']) -> float:
+    # function implementation
+    return df_table1[doc][index] * df_table2['idf'][index]
+
+for doc in ['Doc1', 'Doc2', 'Doc3']:
+    for index in ['car', 'auto', 'insurance', 'best']:
+        print(f"tf_idf({doc}, {index}) = {tf_idf(doc,index)}")
