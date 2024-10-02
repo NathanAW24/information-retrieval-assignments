20f59b3a443ae339c24a44307846f81d402802ac
NathanAW24 <nathan_wiryawan@mymail.sutd.edu.sg>
2024-10-02 21:52:27 +0800

    add table2

--- week3/exercise1.py
+++ week3/exercise1.py
@@ -1,13 +1,22 @@
 import pandas as pd
 
+index = ['car', 'auto', 'insurance', 'best']
+
 table1 = {
     'Doc1': [27, 3, 0, 14],
     'Doc2': [4, 33, 33, 0],
     'Doc3': [24, 0, 29, 17]
 }
 
-index = ['car', 'auto', 'insurance', 'best']
 
 df_table1 = pd.DataFrame(table1, index=index)
 
-print(df_table1)
+# print(df_table1)
+
+table2 = {
+    'idf': [1.65, 2.08, 1.62, 1.5]
+}
+
+df_table2 = pd.DataFrame(table2, index=index)
+
+# print(df_table2)
