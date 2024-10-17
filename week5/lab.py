0d6187068145f156065da80623b2b8072d5fdb9d
NathanAW24 <nathan_wiryawan@mymail.sutd.edu.sg>
2024-10-17 20:34:55 +0800

    dk bener ap kg

--- week5/lab.py
+++ week5/lab.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+from typing import List
+
 import re
 import math
 import numpy as np
@@ -8,12 +11,15 @@ UNK = None
 SENTENCE_START = "<s>"
 SENTENCE_END = "</s>"
 
-def read_sentences_from_file(file_path):
+def read_sentences_from_file(file_path: str) -> List[List[str]]:
     '''
         read the files.
     '''
     with open(file_path, "r") as f:
         return [re.split("\s+", line.rstrip('\n')) for line in f]
+
+vocabs = read_sentences_from_file('./train.vocab.txt')
+vocabs.append([UNK])
 
 class UnigramLanguageModel:
     def __init__(self, sentences, mode="collection", smoothing=False):
@@ -23,16 +29,47 @@ class UnigramLanguageModel:
             mode: whether this language model is for the whole corpus/collection or just a single document
             smoothing: add-one smoothing
         '''
+        self.mode = mode
+        self.smoothing = smoothing
+
+        self.vocab = defaultdict(int)
+
+        for sentence in sentences:
+            for word in sentence:
+                if word != SENTENCE_START and word != SENTENCE_END:
+                    self.vocab[word] += 1
+        
+        if self.mode == "collection":
+            self.vocab[UNK] = 1
+        
+        self.total = sum(self.vocab.values())
+
     def calculate_unigram_probability(self, word):
         '''
             calculate unigram probability of a word
         '''
+        global vocabs
+        if self.smoothing:
+            if self.mode == "doc":
+                return (self.vocab[word] + 1) / (self.total + len(vocabs))
+            if word in self.vocab:
+                count = self.vocab[word]
+            else:
+                count = self.vocab[UNK]
+            
+            return (count + 1) / (self.total + len(vocabs))
+        else:
+            return self.vocab[word] / self.total
+
+
     def calculate_sentence_probability(self, sentence, normalize_probability=True):
         '''
             calculate score/probability of a sentence or query using the unigram language model.
             sentence: input sentence or query
             normalize_probability: If true then log of probability is not computed. Otherwise take log2 of the probability score.
         '''
+        
+        return
 
 def calculate_interpolated_sentence_probability(sentence, doc, collection, alpha=0.75, normalize_probability=True):
     '''
@@ -43,6 +80,7 @@ def calculate_interpolated_sentence_probability(sentence, doc, collection, alpha
         alpha: the hyperparameter to combine the two probability scores coming from the document and collection language models.
         normalize_probability: If true then log of probability is not computed. Otherwise take log2 of the probability score.
     '''
+    return
 
 if __name__ == '__main__':
     # first read the datasets
@@ -52,6 +90,10 @@ if __name__ == '__main__':
     doc2_dataset = read_sentences_from_file("./doc2.txt")
     doc3_dataset = read_sentences_from_file("./doc3.txt")
     actual_dataset_test = read_sentences_from_file("./test.txt")
+
+
+
+    print(doc1_dataset)
 
     '''
         Question: for each of the test queries given in test.txt, find out best matching document/doc
