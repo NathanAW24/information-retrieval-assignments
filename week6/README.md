2a1e61f1b98c01a7b4a6a3c067ff879b23c46cd1
NathanAW24 <nathan_wiryawan@mymail.sutd.edu.sg>
2024-10-26 14:56:57 +0800

    init week 6 assignment

---
+++ week6/README.md
@@ -0,0 +1,6 @@
+# Precision
+```
+precision = <number-of-relevant-docs-retrieved>/<total-number-of-docs-retrieved>
+```
+
+# Mean averate precision
---
+++ week6/exercise.py
@@ -0,0 +1,147 @@
+import numpy as np
+
+def precision_at_k(r, k):
+    """Score is precision @ k (This we solve for you!)
+    Relevance is binary (nonzero is relevant).
+    >>> r = [0, 0, 1]
+    >>> precision_at_k(r, 1)
+    0.0
+    >>> precision_at_k(r, 2)
+    0.0
+    >>> precision_at_k(r, 3)
+    0.33333333333333331
+    >>> precision_at_k(r, 4)
+    Traceback (most recent call last):
+    File "<stdin>", line 1, in ?
+    ValueError: Relevance score length < k
+    Args:
+    r: Relevance scores (list or numpy) in rank order
+    (first element is the first item)
+    Returns:
+    Precision @ k
+    Raises:
+    ValueError: len(r) must be >= k
+    """
+    assert k >= 1
+    r = np.asarray(r)[:k] != 0
+    if r.size != k:
+        raise ValueError('Relevance score length < k')
+    return np.mean(r)
+
+def average_precision(r):
+    """Score is average precision (area under PR curve)
+    Relevance is binary (nonzero is relevant).
+    >>> r = [1, 1, 0, 1, 0, 1, 0, 0, 0, 1]
+    >>> delta_r = 1. / sum(r)
+    >>> sum([sum(r[:x + 1]) / (x + 1.) * delta_r for x, y in
+    enumerate(r) if y])
+    0.7833333333333333
+    >>> average_precision(r)
+    0.78333333333333333
+    Args:
+    r: Relevance scores (list or numpy) in rank order
+    (first element is the first item)
+    Returns:
+    Average precision
+    """
+    #write your code here
+    avg_p = 0
+    return avg_p
+
+def mean_average_precision(rs):
+    """Score is mean average precision
+    Relevance is binary (nonzero is relevant).
+    >>> rs = [[1, 1, 0, 1, 0, 1, 0, 0, 0, 1]]
+    >>> mean_average_precision(rs)
+    0.78333333333333333
+    >>> rs = [[1, 1, 0, 1, 0, 1, 0, 0, 0, 1], [0]]
+    >>> mean_average_precision(rs)
+    0.39166666666666666
+    Args:
+    rs: Iterator of relevance scores (list or numpy) in rank order
+    (first element is the first item)
+    Returns:
+    Mean average precision
+    """
+    #write your code here
+    m_avg_p = 0
+    return m_avg_p
+
+
+def dcg_at_k(r, k, method=0):
+    """Score is discounted cumulative gain (dcg)
+    Relevance is positive real values. Can use binary
+    as the previous methods.
+    Example from
+    http://www.stanford.edu/class/cs276/handouts/EvaluationNew-handout-6-per
+    .pdf
+    >>> r = [3, 2, 3, 0, 0, 1, 2, 2, 3, 0]
+    >>> dcg_at_k(r, 1)
+    3.0
+    >>> dcg_at_k(r, 1, method=1)
+    3.0
+    >>> dcg_at_k(r, 2)
+    5.0
+    >>> dcg_at_k(r, 2, method=1)
+    4.2618595071429155
+    >>> dcg_at_k(r, 10)
+    9.6051177391888114
+    >>> dcg_at_k(r, 11)
+    9.6051177391888114
+    Args:
+    r: Relevance scores (list or numpy) in rank order
+    (first element is the first item)
+    k: Number of results to consider
+    method: If 0 then weights are [1.0, 1.0, 0.6309, 0.5, 0.4307,
+    ...]
+    If 1 then weights are [1.0, 0.6309, 0.5, 0.4307, ...]
+    Returns:
+    Discounted cumulative gain
+    """
+    #write your code here (return appropriate value)
+    return 0
+
+def ndcg_at_k(r, k, method=0):
+    """Score is normalized discounted cumulative gain (ndcg)
+    Relevance is positive real values. Can use binary
+    as the previous methods.
+    Example from
+    http://www.stanford.edu/class/cs276/handouts/EvaluationNew-handout-6-per
+    .pdf
+    >>> r = [3, 2, 3, 0, 0, 1, 2, 2, 3, 0]
+    >>> ndcg_at_k(r, 1)
+    1.0
+    >>> r = [2, 1, 2, 0]
+    >>> ndcg_at_k(r, 4)
+    0.9203032077642922
+    >>> ndcg_at_k(r, 4, method=1)
+    0.96519546960144276
+    >>> ndcg_at_k([0], 1)
+    0.0
+    >>> ndcg_at_k([1], 2)
+    1.0
+    Args:
+    r: Relevance scores (list or numpy) in rank order
+    (first element is the first item)
+    k: Number of results to consider
+    method: If 0 then weights are [1.0, 1.0, 0.6309, 0.5, 0.4307,
+    ...]
+    If 1 then weights are [1.0, 0.6309, 0.5, 0.4307, ...]
+    Returns:
+    Normalized discounted cumulative gain
+    """
+    #write your code here (return appropriate value)
+    return 0.
+
+if __name__ == "__main__":
+    print(precision_at_k([0, 0, 1],1))
+    print(precision_at_k([0, 0, 1],2))
+    print(precision_at_k([0, 0, 1],3))
+    print(precision_at_k([0, 0, 1],4))
+
+
+    print(average_precision([0,1,0,0,1,0,1,0,0,0]))
+    print(average_precision([1,0,1,0,0,1,0,0,1,1]))
+    print(mean_average_precision([[1,0,1,0,0,1,0,0,1,1],[0,1,0,0,1,0,1,0,0,0]]))
+    print(dcg_at_k([1.0,0.6,0.0,0.8,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.2,0.0],3))
+    print(ndcg_at_k([1.0,0.6,0.0,0.8,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.2,0.0],3))
