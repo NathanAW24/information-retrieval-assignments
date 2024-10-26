e589922f6bd744686e2001a10292373181da42b7
NathanAW24 <nathan_wiryawan@mymail.sutd.edu.sg>
2024-10-26 15:56:39 +0800

    fix dcg at k

--- week6/exercise.py
+++ week6/exercise.py
@@ -92,12 +92,20 @@ def dcg_at_k(r, k, method=0):
     Returns:
     Discounted cumulative gain
     """
-    r = np.asfarray(r)[:k]
+    r = np.asarray(r, dtype=float)[:k]
     if r.size:
         if method == 0:
-            return r[0] + np.sum(r[1:] / np.log2(np.arange(2, r.size + 2)))
+            # First position is not discounted
+            dcg = r[0]
+            if r.size > 1:
+                dcg += np.sum(r[1:] / np.log2(np.arange(2, r.size + 1)))
+            return dcg
         elif method == 1:
-            return np.sum(r / np.log2(np.arange(2, r.size + 2)))
+            # All positions are discounted
+            dcg = np.sum(r / np.log2(np.arange(1, r.size + 1) + 1))
+            return dcg
+        else:
+            raise ValueError('method must be 0 or 1.')
     return 0.
 
 def ndcg_at_k(r, k, method=0):
@@ -129,8 +137,12 @@ def ndcg_at_k(r, k, method=0):
     Returns:
     Normalized discounted cumulative gain
     """
-    #write your code here (return appropriate value)
-    return 0.
+    # dcg_max = dcg_at_k(sorted(r, reverse=True), k, method)
+    # if not dcg_max:
+    #     return 0.
+    # return dcg_at_k(r, k, method) / dcg_max
+    return
+
 
 if __name__ == "__main__":
     print(precision_at_k([0, 0, 1],1))
